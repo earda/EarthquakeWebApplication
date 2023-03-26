75f1c5826c8f3c8f1e29e52e76c192116539eb77
Erdem <earda230@gmail.com>
2023-03-26 19:09:09 +0300

    init project
    init project

---
+++ earthquakewebapplication/src/main/java/com/project/earthquake/earthquakewebapplication/EarthquakewebapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.project.earthquake.earthquakewebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakewebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakewebapplicationApplication.class, args);
+	}
+
+}
